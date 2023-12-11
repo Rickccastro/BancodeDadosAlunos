32b9004ce44535c0c40905e6d6740f2c300b1a9d
Ricardo Parmezan de Almeida Castro <127992895+Rickccastro@users.noreply.github.com>
2023-12-11 11:48:38 -0300

    Delete NaveRefatoradaObserver directory

--- NaveRefatoradaObserver/src/main/java/br/com/mariojp/game/Subject.java
+++
@@ -1,9 +0,0 @@
-package br.com.mariojp.game;
-
-public interface Subject {
-
-	 void addObserver(Observer observer);
-	 void removeObserver(Observer observer);
-	 void notifyObservers(String message);
-	 void notifyObservers(String message, int x);
-}
